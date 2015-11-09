7e63ce3b7a8318c6dd4f8e0175db9afbc4f6493a
Tim O'Shea <tim.oshea753@gmail.com>
2015-11-08 19:37:27 -0500

    making progress with benchmark plot

--- apps/benchmark.py
+++ apps/benchmark.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from subprocess import Popen, PIPE
-import time, subprocess
+import time, subprocess,sys,json
 
 apps = [{"name": "Single Sink",
         "f": "test_sim.py"},
@@ -8,25 +8,37 @@ apps = [{"name": "Single Sink",
         "f": "test_sim_multi.py"}]
 
 simlen_sec = 30
-nthreads = [1,2,3,4,5,6,7,8];
+#nthreads = [1,2,4];
+#nthreads = [1,2,3,4,5,6,7,8];
+nthreads = [1,2,4,6,8];
 ntrials = 2
 py = "/usr/bin/python"
+nstat = 10 # number of status updates to wait
+numlines = 2+nstat*7
+
+records = []
 for nt  in nthreads:
     for trial in range(0,ntrials):
         for app in apps:
             print "RUNNING", nt, trial, app           
-            p = subprocess.Popen([py, app['f'], '--t=%d'%(simlen_sec), '--nthreads=%d'%(nt)], stdout=subprocess.PIPE)
-            #p = subprocess.Popen([py, app['f'], '--t=%d'%(simlen_sec), '--nthreads=%d'%(nt)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            time.sleep(5)
+            p = subprocess.Popen([py, app['f'], '--nthreads=%d'%(nt)], stdout=subprocess.PIPE)
+            op = []
+            for i in range(0,numlines):
+                o = p.stdout.readline() 
+                op.append(o)
+            print op
             p.kill()
-            
-            o = p.stdout.read() 
-            print o
-            
-            #print p.stdout.read()
-            #print p.stdout.read()
-#            output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-#            rc = p.returncode
-#            print output
             print "DONE"
+    
+            records.append(
+                {"nthreads":nt,
+                 "trial":trial,
+                 "f":app["f"],
+                 "d":app["name"],
+                  "output":op} 
+                    )
 
+print "WRITING RESULTS!"
+outf = open("stats.dat","w")
+outf.write( json.dumps(records) )
+print "finished..."
