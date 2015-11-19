5357e8bef02127833d55eef3d7dec700ab4ed6f2
Tim O'Shea <tim.oshea753@gmail.com>
2015-11-19 11:24:00 -0500

    checking in applications

---
+++ apps/test_data_gen.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Test Data Gen
+# Generated: Mon Nov  9 20:12:31 2015
+##################################################
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+
+class test_data_gen(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Test Data Gen")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 25e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, int(samp_rate*10))
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, "test.dat", False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.analog_fastnoise_source_x_0 = analog.fastnoise_source_c(analog.GR_GAUSSIAN, 1, 0, 8192)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_fastnoise_source_x_0, 0), (self.blocks_head_0, 0))    
+        self.connect((self.blocks_head_0, 0), (self.blocks_file_sink_0, 0))    
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_head_0.set_length(int(self.samp_rate*10))
+
+
+if __name__ == '__main__':
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = test_data_gen()
+    tb.start()
+    tb.wait()
---
+++ apps/test_sim_multi.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python2
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Test Sim Multi
+# Generated: Thu Nov 19 11:23:28 2015
+##################################################
+
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import es
+import eventsim
+
+class test_sim_multi(gr.top_block):
+
+    def __init__(self, nthreads=4, dps=500, l2=15.0, l1=10.0, ld=4.0):
+        gr.top_block.__init__(self, "Test Sim Multi")
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.nthreads = nthreads
+        self.dps = dps
+        self.l2 = l2
+        self.l1 = l1
+        self.ld = ld
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 25e6
+        self.dbps = dbps = 10
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.eventsim_operation2_0 = eventsim.operation2(samp_rate, 0, l2)
+        self.eventsim_operation1_0 = eventsim.operation1(samp_rate, 0, l1)
+        self.eventsim_detector_0 = eventsim.detector(samp_rate, dps, ld)
+        self.es_trigger_sample_timer_0 = es.trigger_sample_timer(gr.sizeof_gr_complex, int(samp_rate/dbps), 10000, int(samp_rate), int(samp_rate/dbps) )
+        (self.es_trigger_sample_timer_0).set_min_output_buffer(8000000)
+        self.es_sink_1 = es.sink(1*[gr.sizeof_gr_complex],nthreads,256,0,2,"group1")
+        self.es_sink_0 = es.sink(1*[gr.sizeof_gr_complex],nthreads,256,2,2,"group1")
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, "test.dat", True)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.es_trigger_sample_timer_0, 'which_stream'), (self.es_sink_1, 'schedule_event'))    
+        self.msg_connect((self.es_trigger_sample_timer_0, 'sample_timer_event'), (self.eventsim_detector_0, 'handle_event'))    
+        self.msg_connect((self.eventsim_detector_0, 'which_stream'), (self.es_sink_1, 'schedule_event'))    
+        self.msg_connect((self.eventsim_detector_0, 'detect_type1_event'), (self.eventsim_operation1_0, 'handle_event'))    
+        self.msg_connect((self.eventsim_detector_0, 'detect_type2_event'), (self.eventsim_operation2_0, 'handle_event'))    
+        self.msg_connect((self.eventsim_operation1_0, 'type1_event'), (self.eventsim_operation2_0, 'handle_event'))    
+        self.msg_connect((self.eventsim_operation2_0, 'which_stream'), (self.es_sink_1, 'schedule_event'))    
+        self.connect((self.blocks_file_source_0, 0), (self.es_trigger_sample_timer_0, 0))    
+        self.connect((self.es_trigger_sample_timer_0, 0), (self.es_sink_0, 0))    
+        self.connect((self.es_trigger_sample_timer_0, 0), (self.es_sink_1, 0))    
+
+
+    def get_nthreads(self):
+        return self.nthreads
+
+    def set_nthreads(self, nthreads):
+        self.nthreads = nthreads
+
+    def get_dps(self):
+        return self.dps
+
+    def set_dps(self, dps):
+        self.dps = dps
+
+    def get_l2(self):
+        return self.l2
+
+    def set_l2(self, l2):
+        self.l2 = l2
+
+    def get_l1(self):
+        return self.l1
+
+    def set_l1(self, l1):
+        self.l1 = l1
+
+    def get_ld(self):
+        return self.ld
+
+    def set_ld(self, ld):
+        self.ld = ld
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+    def get_dbps(self):
+        return self.dbps
+
+    def set_dbps(self, dbps):
+        self.dbps = dbps
+
+
+if __name__ == '__main__':
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    parser.add_option("", "--nthreads", dest="nthreads", type="intx", default=4,
+        help="Set nthreads [default=%default]")
+    parser.add_option("", "--dps", dest="dps", type="eng_float", default=eng_notation.num_to_str(500),
+        help="Set dps [default=%default]")
+    parser.add_option("", "--l2", dest="l2", type="eng_float", default=eng_notation.num_to_str(15.0),
+        help="Set l2 [default=%default]")
+    parser.add_option("", "--l1", dest="l1", type="eng_float", default=eng_notation.num_to_str(10.0),
+        help="Set l1 [default=%default]")
+    parser.add_option("", "--ld", dest="ld", type="eng_float", default=eng_notation.num_to_str(4.0),
+        help="Set ld [default=%default]")
+    (options, args) = parser.parse_args()
+    tb = test_sim_multi(nthreads=options.nthreads, dps=options.dps, l2=options.l2, l1=options.l1, ld=options.ld)
+    tb.start()
+    tb.wait()
